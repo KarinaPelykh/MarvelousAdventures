826d2544f53a2faeaad340499b8d6180a74439a3
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2024-08-26 21:52:10 +0200

    add:lisst comics changes color buttons

--- src/js/comics.js
+++ src/js/comics.js
@@ -1,50 +1,83 @@
 import axios from "axios";
-import CryptoJS, { format } from "crypto-js";
+import CryptoJS from "crypto-js";
+
+const ref = {
+  buttonPrev: document.querySelector(".js-previous"),
+  buttonNext: document.querySelector(".js-next"),
+  wrapper: document.querySelector(".js-button-wrapper"),
+};
+const list = document.querySelector(".js-comics");
+
 const API_KEY_PRIVATE = "86167992f51495ba975666074c2de2488a64fb00";
 const API_KEY_PUBLIC = "7f8ef27ce3f21548c1d09757433025a4";
-const BASE_URL = "https://gateway.marvel.com";
-const list = document.querySelector(".js-comics");
+const BASE_URL = "https://gateway.marvel.com:443";
 const TS = "karina";
-const HASH = CryptoJS.MD5(TS + API_KEY_PRIVATE + API_KEY_PUBLIC).toString();
-const url = `${BASE_URL}/v1/public/comics?`;
 
-const handelAddHtml = ({ title, creators, images }) => {
-  return `<li class="">
-             <img src=${images.path} alt="marvel hero"/>
+const HASH = CryptoJS.MD5(TS + API_KEY_PRIVATE + API_KEY_PUBLIC).toString();
+const url = `${BASE_URL}/v1/public/comics?apikey=${API_KEY_PUBLIC}&hash=${HASH}&ts=${TS}`;
+
+let page = 2;
+const limit = 3;
+
+const defaultPhoto =
+  "https://cdn.marvel.com/u/prod//i/mg/3/a0/6451109c83f5f/portrait_uncanny.jpg";
+const handelAddHtml = (results) => {
+  const item = results
+    .map(({ title, creators, images }) => {
+      const imgUrl = images[0]?.path
+        ? `${images[0]?.path}.${images[0]?.extension}`
+        : defaultPhoto;
+      const name = creators.items ? creators.items[0]?.name : "-";
+      return `<li class="item-comics">
+             <img class="img-comics" src=${imgUrl} alt="marvel hero"/>
                   <div class="">
                  <h3>${title}</h3>
-                 <p>${creators}</p>
+                 <p>${name}</p>
              </div>
             </li>`;
+    })
+    .join("");
+
+  list.innerHTML = item;
 };
 
-const fetchAuthorization = () => {
+ref.wrapper.style.display = "none";
+const fetchAuthorization = async (offset) => {
   axios
-    .get(url, {
-      params: {
-        ts: TS,
-        format: "comic",
-        apikey: API_KEY_PUBLIC,
-        hash: HASH,
-      },
-    })
+    .get(`${url}&limit=${limit}&offset=${offset}`)
     .then(
       ({
         data: {
           data: { results },
         },
       }) => {
-        results.forEach(({ title, creators, images }) => {
-          console.log({ title, creators, images });
+        if (results) {
+          ref.wrapper.style.display = "flex";
+        }
 
-          //   const item = handelAddHtml({ title, creators, images });
-          //   list.insertAdjacentElement("beforeend", item);
-        });
-        console.log(results);
+        handelAddHtml(results);
       }
     )
     .catch((error) => {
       console.log(error);
     });
 };
+
+const handelNext = () => {
+  page += 1;
+
+  const offset = (page - 1) * 3;
+
+  fetchAuthorization(offset);
+};
+const handelPrevious = () => {
+  if (page > 1) {
+    page -= 1;
+    const offset = (page - 1) * 3;
+    fetchAuthorization(offset);
+  }
+};
 fetchAuthorization();
+
+ref.buttonPrev.addEventListener("click", handelPrevious);
+ref.buttonNext.addEventListener("click", handelNext);
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -2,7 +2,11 @@ import { marvelHero } from "../../public/data";
 const gallery = document.querySelector(".gallery");
 const pagination = document.querySelector(".pagination");
 const buttonLink = document.querySelector(".js-button");
+const buttonPrev = document.querySelector(".js-previous");
 
+let currentIndex = 0;
+const buttons = [];
+// add html
 const handelAddHtml = ({ img1, img2, text, name }) => {
   return `<li class="item"  data-action=${name}>
              <div class="slid-hero">
@@ -17,16 +21,21 @@ const handelAddHtml = ({ img1, img2, text, name }) => {
              </div>
             </li>`;
 };
+// add html slide
+const handelAddHtmlOnPage = () => {
+  const html = marvelHero
+    .map((data) => {
+      return handelAddHtml(data);
+    })
+    .join("");
 
-const html = marvelHero.map((data) => {
-  return handelAddHtml(data);
-});
-
-gallery.innerHTML = html.join("");
+  gallery.innerHTML = html;
+};
+handelAddHtmlOnPage();
 
 const slides = document.querySelectorAll(".gallery .item");
-let currentIndex = 0;
 
+//update slide
 const handelUpdateImg = () => {
   const offset = -currentIndex * 100;
   slides.forEach((slide) => {
@@ -36,15 +45,60 @@ const handelUpdateImg = () => {
         : `translateX(${offset}%)`;
   });
 };
+
+// autoswitch slide
+const handelAuto = () => {
+  const prevIndex = (currentIndex - 1 + slides.length) % slides.length;
+  currentIndex = (currentIndex + 1) % slides.length;
+
+  handelUpdateImg();
+  handelChangeColor(prevIndex);
+  handleChangeColorButton(prevIndex);
+};
+
+setInterval(handelAuto, 3000);
+
+// add pagination
+const handelAddPAgination = () => {
+  slides.forEach((_, index) => {
+    const button = document.createElement("button");
+    button.addEventListener("click", () => {
+      currentIndex = index;
+      handelUpdateImg();
+    });
+    pagination.appendChild(button);
+    buttons.push(button);
+  });
+};
+handelAddPAgination();
+
+// change color button pagination slide
+const handleChangeColorButton = (prevIndex) => {
+  buttons.forEach((button, index) => {
+    if (index === currentIndex) {
+      const background =
+        (prevIndex == 0 && "#600404") ||
+        (prevIndex == 1 && "#34387F") ||
+        (prevIndex == 2 && "#5B7F3C");
+      button.style.background = background;
+    } else {
+      button.style.background = "white";
+    }
+  });
+};
+// change color link and pagination
 const handelChangeColor = (prevIndex) => {
   switch (prevIndex) {
     case 0:
+      buttonPrev.style.backgroundColor = "#600404";
       buttonLink.style.backgroundColor = "#600404";
       break;
     case 1:
+      buttonPrev.style.backgroundColor = "#34387F";
       buttonLink.style.backgroundColor = "#34387F";
       break;
     case 2:
+      buttonPrev.style.backgroundColor = "#5B7F3C";
       buttonLink.style.backgroundColor = "#5B7F3C";
       break;
     default:
@@ -53,20 +107,13 @@ const handelChangeColor = (prevIndex) => {
   }
 };
 
-const handelAuto = () => {
-  const prevIndex = (currentIndex - 1 + slides.length) % slides.length;
-  currentIndex = (currentIndex + 1) % slides.length;
-  handelUpdateImg();
-  handelChangeColor(prevIndex);
-};
-
-setInterval(handelAuto, 3000);
-
-const handelAddPAgination = () => {
-  slides.forEach(() => {
-    const button = document.createElement("button");
-    pagination.appendChild(button);
+// change color background
+const bg = ["../img/pantera.png", "../img/hulk-gr.png", "../img/spider.png"];
+const handelChangeBg = () => {
+  const items = document.querySelectorAll(".item .block-info");
+  items.forEach((el, index) => {
+    const background = bg[index % bg.length];
+    el.style.backgroundImage = `url(${background})`;
   });
 };
-
-handelAddPAgination();
+handelChangeBg();
